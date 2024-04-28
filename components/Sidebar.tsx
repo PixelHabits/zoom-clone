76848dc0de57aa41114ea895280312a9c4b92581
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-04-28 18:09:19 -0400

    Added Components Directory & Made Sidebar UI

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,39 @@
+"use client";
+
 import React from "react";
 
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
 const Sidebar = () => {
-	return <div>Sidebar</div>;
+	const pathname = usePathname();
+	return (
+		<section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+			<div className="flex flex- flex-col gap-6">
+				{sidebarLinks.map((link) => {
+					const isActive =
+						pathname === link.route || pathname.startsWith(link.route);
+
+					return (
+						<Link
+							href={link.route}
+							key={link.label}
+							className={cn(
+								"flex gap-4 items-center p-4 rounded-lg justify-start",
+								{
+									"bg-blue-1": isActive,
+								}
+							)}
+						>
+							{link.label}
+						</Link>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Sidebar;
---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+	{
+		label: "Home",
+		route: "/home",
+		imgUrl: "icons/Home.svg",
+	},
+	{
+		label: "Previous",
+		route: "/previous",
+		imgUrl: "icons/previous.svg",
+	},
+	{
+		label: "Recordings",
+		route: "/recordings",
+		imgUrl: "icons/Video.svg",
+	},
+	{
+		label: "Personal Room",
+		route: "/personal-room",
+		imgUrl: "icons/add-=personal.svg",
+	},
+];
