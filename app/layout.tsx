1daaa62e61601f3a653a2ab2c2625488df483406
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-04-28 23:37:08 -0400

    Auth Page Styling Complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Darker_Grotesque, Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
+import { dark } from '@clerk/themes';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,26 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<ClerkProvider>
+			<ClerkProvider
+				appearance={{
+					elements: {
+						socialButtonsIconButton: 'bg-zinc-950',
+						socialButtonsProviderIcon__github: 'invert',
+					},
+					layout: {
+						socialButtonsVariant: 'iconButton',
+						logoImageUrl: 'icons/yoom-logo.svg',
+					},
+					baseTheme: dark,
+					variables: {
+						colorText: '#fff',
+						colorPrimary: '#0E78F9',
+						colorBackground: '#1c1f2e',
+						colorInputBackground: '#252a41',
+						colorInputText: '#fff',
+					},
+				}}
+			>
 				<body className={`${inter.className} bg-dark-2`}>{children}</body>
 			</ClerkProvider>
 		</html>
