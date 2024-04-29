d42ec7ec39b4facba05fe51e89ddfb6701db70d0
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-04-28 23:01:59 -0400

    Auth Working & Initial Styling Done

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/nextjs';
 
-export default function Page() {
-	return <SignIn path='/sign-in' />;
+export default function SignInPage() {
+	return (
+		<main className='flex h-screen w-full items-center justify-center'>
+			<SignIn />
+		</main>
+	);
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs';
 
-export default function Page() {
-	return <SignUp path='/sign-up' />;
+export default function SignInPage() {
+	return (
+		<main className='flex h-screen w-full items-center justify-center'>
+			<SignUp />
+		</main>
+	);
 }
