87d17512a183146f3c7ce48b04c2ae67024b10a2
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-04-30 22:27:53 -0400

    Adjusted StreamClientProvider to be Client Side Component

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { tokenProvider } from '@/actions/stream.actions';
 import Loader from '@/components/Loader';
 import { useUser } from '@clerk/nextjs';
@@ -8,28 +10,26 @@ const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
 export const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
 	const [videoClient, setVideoClient] = useState<StreamVideoClient>();
-    const {user, isLoaded } = useUser();
-    useEffect(() => {
-        if(!isLoaded || !user) return;
-        if(!apiKey) throw new Error('Stream API key missing');
+	const { user, isLoaded } = useUser();
+	useEffect(() => {
+		if (!isLoaded || !user) return;
+		if (!apiKey) throw new Error('Stream API key missing');
 
-        const client = new StreamVideoClient ({
-            apiKey,
-            user: {
-                id: user?.id,
-                name: user?.username || user?.id,
-                image: user?.imageUrl,
-            },
-            tokenProvider,
-        })
-        
-        setVideoClient(client);
-    }, [user, isLoaded]);
+		const client = new StreamVideoClient({
+			apiKey,
+			user: {
+				id: user?.id,
+				name: user?.username || user?.id,
+				image: user?.imageUrl,
+			},
+			tokenProvider,
+		});
 
-    if(!videoClient) return <Loader />
-	return <StreamVideo client={videoClient}>
-        {children}
-    </StreamVideo>;
+		setVideoClient(client);
+	}, [user, isLoaded]);
+
+	if (!videoClient) return <Loader />;
+	return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 };
 
 export default StreamVideoProvider;
