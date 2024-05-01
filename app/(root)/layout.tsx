87726b152cd0bde2027bdc8eee02bda2a77c63a7
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-04-30 22:23:33 -0400

    Added StreamVideoProvider Component & Token Provider Server Action

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
-import React, { ReactNode } from "react";
+import StreamVideoProvider from '@/providers/StreamClientProvider';
+import React, { ReactNode } from 'react';
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-	return <main>{children}</main>;
+	return (
+		<main>
+			<StreamVideoProvider>{children}</StreamVideoProvider>
+		</main>
+	);
 };
 
 export default RootLayout;
