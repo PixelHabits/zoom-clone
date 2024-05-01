63b037200aa80ab9fbfeb2e176a94efb35de01b4
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-05-01 01:51:50 -0400

    Added useEffect To Prompt Camera Permissions

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+'use client';
+
+import { VideoPreview, useCall } from '@stream-io/video-react-sdk';
+import React, { useEffect, useState } from 'react';
 
 const MeetingSetup = () => {
-  return (
-    <div>MeetingSetup</div>
-  )
-}
+	const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
 
-export default MeetingSetup
+	const call = useCall();
+
+	if (!call) {
+		throw new Error('usecall must be used within StreamCall component');
+	}
+
+	useEffect(() => {
+		console.log('Started useEffect');
+		if (isMicCamToggledOn) {
+			console.log('if statement');
+			call?.camera.disable();
+			call?.microphone.disable();
+		} else {
+			console.log('else statement');
+			call?.camera.enable();
+			call?.microphone.enable();
+		}
+	}, [isMicCamToggledOn, call?.camera, call?.microphone]);
+
+	return (
+		<div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
+			<h1 className='text-2xl font-bold'>Setup</h1>
+			<VideoPreview />
+		</div>
+	);
+};
+
+export default MeetingSetup;
