2d9a277a5e357b6f2a234ae8599e7f2e2bb53ae6
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-05-01 10:14:15 -0400

    Added Loader to MeetingRoom Component, flex-wrap Styling to Meeting Controls

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -3,8 +3,10 @@ import {
 	CallControls,
 	CallParticipantsList,
 	CallStatsButton,
+	CallingState,
 	PaginatedGridLayout,
 	SpeakerLayout,
+    useCallStateHooks,
 } from '@stream-io/video-react-sdk';
 import React, { useState } from 'react';
 
@@ -16,7 +18,7 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { LayoutList, Users } from 'lucide-react';
+import { LayoutList, Loader, Users } from 'lucide-react';
 import { useSearchParams } from 'next/navigation';
 import EndCallButton from './EndCallButton';
 
@@ -28,6 +30,14 @@ const MeetingRoom = () => {
 	const [layout, setLayout] = useState('speaker-left');
 
 	const [showParticipants, setShowParticipants] = useState(false);
+
+    const { useCallCallingState } = useCallStateHooks();
+    const callingState = useCallCallingState();
+    if(callingState !== CallingState.JOINED) return;
+    <Loader />
+
+
+    
 	const CallLayout = () => {
 		switch (layout) {
 			case 'grid':
@@ -53,7 +63,7 @@ const MeetingRoom = () => {
 				</div>
 			</div>
 
-			<div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
+			<div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap'>
 				<CallControls />
 				<DropdownMenu>
 					<div className='flex items-center'>
